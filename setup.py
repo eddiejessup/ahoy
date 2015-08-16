3304e5e2bfc9f5b130473170631f56184a674c0d
Elliot Marsden <elliot.marsden@gmail.com>
2015-08-16 13:04:56 +0100

    Bump version to v0.4.0

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ console_scripts = [
 
 setup(
     name='ahoy',
-    version='0.3.0',
+    version='0.4.0',
     description="Agent-based simulations of active particles",
     long_description=readme + '\n\n' + history,
     author="Elliot Marsden",
